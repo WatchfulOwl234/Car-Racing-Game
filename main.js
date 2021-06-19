91a57df0494b5a3c0408648863879aadeed1a390
WatchfulOwl <79873040+WatchfulOwl234@users.noreply.github.com>
2021-06-19 10:02:03 -0700

    Add files via upload

--- main.js
+++ main.js
@@ -5,8 +5,11 @@ rover_width = 100;
 rover_height = 90; 
 background_image = "racing.jpg"; 
 rover_image = "car1.png"; 
+rover1_image = "car2.png";
 rover_x = 10; 
 rover_y = 10;
+rover1_x = 25; 
+rover1_y = 25;
 //--------------------------------------
 function add() 
 { 
@@ -22,6 +25,13 @@ rover_imgTag.onload = uploadrover;
 // setting a function, onloading this variable 
 rover_imgTag.src = rover_image; 
 // load image 
+
+rover_imgTag1 = new Image(); 
+//defining a variable with a new image 
+rover_imgTag1.onload = uploadrover; 
+// setting a function, onloading this variable 
+rover_imgTag1.src = rover1_image; 
+// load image 
 }
 //----------------------------------------
 function uploadBackground() 
@@ -32,6 +42,8 @@ ctx.drawImage(background_imgTag, 0, 0, canvas.width, canvas.height);
 function uploadrover() 
 { 
 ctx.drawImage(rover_imgTag, rover_x, rover_y, rover_width, rover_height); 
+ctx.drawImage(rover_imgTag1, rover1_x, rover1_y, rover_width, rover_height); 
+
 }
 //----------------------------------------
 window.addEventListener("keydown", my_keydown); 
@@ -57,6 +69,25 @@ if(keyPressed == '39')
 { right(); 
 console.log("right"); 
 } 
+if(keyPressed == '87') 
+{ 
+letterW(); 
+console.log("upW"); 
+} 
+if(keyPressed == '65') 
+{ 
+letterA(); 
+console.log("leftA"); 
+}
+ if(keyPressed == '83') 
+{
+letterS(); 
+console.log("downS"); 
+} 
+if(keyPressed == '68') 
+{ letterD(); 
+console.log("rightD"); 
+} 
 }
 //--------------------------------------------
 function up() 
@@ -65,6 +96,17 @@ if(rover_y >=0)
 { 
 rover_y = rover_y - 10; 
 console.log("When up arrow is pressed, x = " + rover_x + " | y = " +rover_y); 
+uploadBackground(); 
+uploadrover(); 
+} 
+}
+
+function letterW() 
+{ 
+if(rover1_y >=0) 
+{ 
+rover1_y = rover1_y - 10; 
+console.log("When W Key is pressed, x = " + rover1_x + " | y = " +rover1_y); 
 uploadBackground(); 
 uploadrover(); 
 } 
@@ -81,12 +123,34 @@ uploadrover();
 } 
 }
 
+function letterS() 
+{ 
+if(rover1_y <=500) 
+{ 
+rover1_y = rover1_y + 10; 
+console.log("When S key is pressed, x = " + rover1_x + " | y = " +rover1_y); 
+uploadBackground(); 
+uploadrover(); 
+} 
+}
+
+
 function left() 
 { 
 if(rover_x >=0) 
 { 
 rover_x = rover_x - 10; 
 console.log("When left arrow is pressed, x = " + rover_x + " | y = " +rover_y); 
+uploadBackground(); 
+uploadrover(); 
+} 
+}
+function letterA() 
+{ 
+if(rover1_x >=0) 
+{ 
+rover1_x = rover1_x - 10; 
+console.log("When A key is pressed, x = " + rover1_x + " | y = " +rover1_y); 
 uploadBackground(); 
 uploadrover(); 
 } 
@@ -102,6 +166,30 @@ uploadBackground();
 uploadrover(); 
 } 
 }
+
+function letterD() 
+{ 
+if(rover1_x <=700) 
+{ 
+rover1_x = rover1_x + 10; 
+console.log("When D key is pressed, x = " + rover1_x + " | y = " +rover1_y); 
+uploadBackground(); 
+uploadrover(); 
+} 
+}
+//----------------------------------------------------
+if(rover_x > 700)
+{
+    console.log ("Car 1 Won!!!");
+    document.getElementById("Title").innerHTML="Car 1 Won!!!";
+}
+
+if(rover1_x > 700)
+{
+    console.log ("Car 2 Won!!!");
+    document.getElementById("Title").innerHTML="Car 2 Won!!!";
+}
+
 
 
 
